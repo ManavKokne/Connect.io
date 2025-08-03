515fec7e398d6976a9fd5bf7ec6bad1f7bb351a0
ManavKokne <koknemanav@gmail.com>
2025-08-03 23:16:00 +0530

    D2_Updates

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,53 @@
-export const signup = (req,res)=>{
-    res.send("signup route")
+import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req,res)=>{
+    const {fullName, email, password} = req.body;
+    try{
+        if(password.length < 6){
+            return res.status(400).json({
+                message : "Password must be at least 6 characters"
+            })
+        }
+
+        const user = await User.findOne({email})
+
+        if(user){
+            return res.status(400).json({
+                message : "Email Already Exists"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User({
+            fullName,
+            email,
+            password : hashedPassword
+        })
+
+        if(newUser){
+            // Generate the JWT token here
+            generateTokens(newUser._id,res);
+            await newUser.save();
+
+            res.status(201).json({
+                newUser,
+                message : "New User Created Successfully"
+            })
+        }else{
+            res.status(400).json({
+                message : "Invalid User Data"
+            })
+        }
+
+    }catch(err){
+        console.log("Error in the signup controller", err.message);
+        res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
 }
 
 export const login = (req,res)=>{
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server is Running on Port : ${PORT}`);
+    connectDB();
 })

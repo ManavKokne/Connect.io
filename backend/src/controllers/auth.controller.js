168f11f85a525853061729383861031a0ee12397
ManavKokne <koknemanav@gmail.com>
2025-08-10 11:36:05 +0530

    added auth controllers for user signup, login, and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,17 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import {generateTokens} from "../lib/utils.js"
 
 export const signup = async (req,res)=>{
     const {fullName, email, password} = req.body;
     try{
+
+        if(!fullName || !email || !password){
+            return res.status(400).json({
+                message : "All the fields are required"
+            })
+        }
+
         if(password.length < 6){
             return res.status(400).json({
                 message : "Password must be at least 6 characters"
@@ -21,7 +29,7 @@ export const signup = async (req,res)=>{
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = await User({
+        const newUser = new User({
             fullName,
             email,
             password : hashedPassword
@@ -50,10 +58,58 @@ export const signup = async (req,res)=>{
     }
 }
 
-export const login = (req,res)=>{
-    res.send("login route")
+export const login = async (req,res)=>{
+    const {email, password} = req.body;
+    
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            res.status(400).json({
+                message : "Invalid Credentials"
+            })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect){
+            res.status(400).json({
+                message : "Invalid Credentials"
+            })
+        }
+
+        generateTokens(user._id, res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email,
+            profilePic : user.profilePic,
+        });
+
+    }catch(err){
+        console.log("Error in the login controller", err.message);
+        res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
 }
 
 export const logout = (req,res)=>{
-    res.send("logout route")
+    try{
+        res.cookie("jwt","", {maxAge : 0})
+
+        res.status(200).json({
+            message : "Logged Out Successfully!"
+        })
+    }catch(err){
+        console.log("Error in the logout controller", err.message);
+        res.status(500).json({
+            message : "Internal Server Error"
+        })
+    }
+}
+
+export const updateProfile = async(req,res) =>{
+    
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,20 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+
+app.get("/",(req, res)=>{
+    res.send("Server is Running")
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is Running on Port : ${PORT}`);
